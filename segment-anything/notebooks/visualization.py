cbd2ca81abc4cd79c9f98a0d66aa0dfea598c4a6
para-lost <18801102729@163.com>
2023-12-31 10:17:33 +0000

    vis code change

--- segment-anything/notebooks/visualization.py
+++ segment-anything/notebooks/visualization.py
@@ -7,7 +7,145 @@ import os
 sys.path.append("..")
 from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
 from PIL import Image
+from monai.losses import DiceCELoss
+from monai.inferers import sliding_window_inference
+from monai.transforms import (
+    AsDiscrete,
+    EnsureChannelFirstd,
+    Compose,
+    CropForegroundd,
+    LoadImaged,
+    Orientationd,
+    RandFlipd,
+    RandCropByPosNegLabeld,
+    RandShiftIntensityd,
+    ScaleIntensityRanged,
+    Spacingd,
+    RandRotate90d,
+)
 
+from monai.config import print_config
+from monai.metrics import DiceMetric
+from monai.networks.nets import UNETR
+
+from monai.data import (
+    DataLoader,
+    CacheDataset,
+    load_decathlon_datalist,
+    decollate_batch,
+)
+
+train_transforms = Compose(
+    [
+        LoadImaged(keys=["image", "label"]),
+        EnsureChannelFirstd(keys=["image", "label"]),
+        Orientationd(keys=["image", "label"], axcodes="RAS"),
+        Spacingd(
+            keys=["image", "label"],
+            pixdim=(1.5, 1.5, 2.0),
+            mode=("bilinear", "nearest"),
+        ),
+        ScaleIntensityRanged(
+            keys=["image"],
+            a_min=-175,
+            a_max=250,
+            b_min=0.0,
+            b_max=1.0,
+            clip=True,
+        ),
+        CropForegroundd(keys=["image", "label"], source_key="image"),
+        RandCropByPosNegLabeld(
+            keys=["image", "label"],
+            label_key="label",
+            spatial_size=(96, 96, 96),
+            pos=1,
+            neg=1,
+            num_samples=4,
+            image_key="image",
+            image_threshold=0,
+        ),
+        RandFlipd(
+            keys=["image", "label"],
+            spatial_axis=[0],
+            prob=0.10,
+        ),
+        RandFlipd(
+            keys=["image", "label"],
+            spatial_axis=[1],
+            prob=0.10,
+        ),
+        RandFlipd(
+            keys=["image", "label"],
+            spatial_axis=[2],
+            prob=0.10,
+        ),
+        RandRotate90d(
+            keys=["image", "label"],
+            prob=0.10,
+            max_k=3,
+        ),
+        RandShiftIntensityd(
+            keys=["image"],
+            offsets=0.10,
+            prob=0.50,
+        ),
+    ]
+)
+val_transforms = Compose(
+    [
+        LoadImaged(keys=["image", "label"]),
+        EnsureChannelFirstd(keys=["image", "label"]),
+        Orientationd(keys=["image", "label"], axcodes="RAS"),
+        Spacingd(
+            keys=["image", "label"],
+            pixdim=(1.5, 1.5, 2.0),
+            mode=("bilinear", "nearest"),
+        ),
+        ScaleIntensityRanged(keys=["image"], a_min=-175, a_max=250, b_min=0.0, b_max=1.0, clip=True),
+        CropForegroundd(keys=["image", "label"], source_key="image"),
+    ]
+)
+data_dir = "/home/jiaxin/ML23Fall/data/images/RawData/Training/"
+split_json = "dataset_0.json"
+
+datasets = data_dir + split_json
+datalist = load_decathlon_datalist(datasets, True, "training")
+val_files = load_decathlon_datalist(datasets, True, "validation")
+train_ds = CacheDataset(
+    data=datalist,
+    transform=train_transforms,
+    cache_num=24,
+    cache_rate=1.0,
+    num_workers=8,
+)
+train_loader = DataLoader(train_ds, batch_size=1, shuffle=True, num_workers=8, pin_memory=True)
+val_ds = CacheDataset(data=val_files, transform=val_transforms, cache_num=6, cache_rate=1.0, num_workers=4)
+val_loader = DataLoader(val_ds, batch_size=1, shuffle=False, num_workers=4, pin_memory=True)
+slice_map = {
+    "img0035.nii.gz": 170,
+    "img0036.nii.gz": 230,
+    "img0037.nii.gz": 204,
+    "img0038.nii.gz": 204,
+    "img0039.nii.gz": 204,
+    "img0040.nii.gz": 180,
+}
+case_num = 0
+img_name = os.path.split(val_ds[case_num]["image"].meta["filename_or_obj"])[1]
+img = val_ds[case_num]["image"]
+label = val_ds[case_num]["label"]
+img_shape = img.shape
+label_shape = label.shape
+print(f"image shape: {img_shape}, label shape: {label_shape}")
+plt.figure("image", (18, 6))
+plt.subplot(1, 2, 1)
+plt.title("image")
+plt.imshow(img[0, :, :, slice_map[img_name]].detach().cpu(), cmap="gray")
+plt.subplot(1, 2, 2)
+plt.title("label")
+plt.imshow(label[0, :, :, slice_map[img_name]].detach().cpu())
+plt.show()
+output_path = './images/image.png'  # Change to your desired path
+plt.savefig(output_path)
 def get_model():
     sam_checkpoint = "../sam_vit_h_4b8939.pth"
     model_type = "vit_h"
@@ -91,12 +229,16 @@ def visualize_slice_with_sam_mask(image_data, true_mask_data, sam_mask_slice, sl
 
     return fig, ax
 
+def main():
+    # Example usage, process data
+    slice_index = 50  # Choose the index of the slice to visualize
+    image = nib.load('/home/jiaxin/ML23Fall/data/images/RawData/Training/img/img0002.nii.gz')
+    label_image = nib.load('/home/jiaxin/ML23Fall/data/images/RawData/Training/label/label0002.nii.gz')
+    print(label_image)
+    image_data = image.get_fdata()
+    label_data = label_image.get_fdata()
+    print("Label data shape:", label_data.shape)
+    print("Unique values in label data slice:", np.unique(label_data))
 
-# Example usage, process data
-slice_index = 50  # Choose the index of the slice to visualize
-image = nib.load('/home/jiaxin/ML23Fall/data/images/RawData/Training/img/img0001.nii.gz')
-label_image = nib.load('/home/jiaxin/ML23Fall/data/images/RawData/Training/label/label0001.nii.gz')
-image_data = image.get_fdata()
-label_data = label_image.get_fdata()
-sam_generated_masks = generate_slice(image_data, slice_index)
-visualize_slice_with_sam_mask(image_data, label_data, sam_generated_masks, slice_index, save_path='./images/image_visualization.png')
+    sam_generated_masks = generate_slice(image_data, slice_index)
+    visualize_slice_with_sam_mask(image_data, label_data, sam_generated_masks, slice_index, save_path='./images/image_visualization.png')
